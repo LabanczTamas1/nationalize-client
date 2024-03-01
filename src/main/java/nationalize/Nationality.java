83985d84393f76c1b16b0c7083257b290e2867cc
Your Name <you@example.com>
2024-03-01 11:15:20 +0100

    performance: changed Nationality to a record class

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -4,34 +4,6 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
-@lombok.Data
-public class Nationality {
-
-    private long count;
-    private String name;
-
-
-
-
-   @lombok.Data
-    public static class Country {
-
-        private String countryId;
-        private float probability;
-
-
-        @Override
-        public String toString() {
-            return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
-        }
-
-    }
-    @Override
-    public String toString() {
-        return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
-    }
-
+public record Nationality(long count, String name, @SerializedName("country") List<Country> countries) {
+    public record Country(String countryId, float probability) {}
 }
